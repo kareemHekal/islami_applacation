a8e6d47f51cf1fe0097a5472f6f15eabc05cf50c
East-Sound <kareemhekal19@gmail.com>
2024-07-30 20:34:49 +0300

    make it responsive

--- lib/home/tabs/sebhaTab.dart
+++ lib/home/tabs/sebhaTab.dart
@@ -15,109 +15,110 @@ class _SebhatabState extends State<Sebhatab> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 4,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  Image.asset("assets/images/head_sebha_logo.png"),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 75),
-                    child: Transform.rotate(
-                        angle: angle,
-                        child: Image.asset("assets/images/body_sebha_logo.png")),
+    return Center(
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics()
+        ,child: Column(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Image.asset("assets/images/head_sebha_logo.png"),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 75),
+                      child: Transform.rotate(
+                          angle: angle,
+                          child: Image.asset("assets/images/body_sebha_logo.png")),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 27),
+                  child: Text("عدد التسبيحات",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodySmall),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 26.0),
+                  child: Center(
+                    child: InkWell(
+                      onTap: () {},
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          return Container(
+                            width: constraints.maxWidth * 0.2, // 20% of the screen width
+                            decoration: BoxDecoration(
+                                color: app_colors.brown,
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Center(
+                                child: Text("$Counter",
+                                    style: Theme.of(context).textTheme.bodySmall),
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    ),
                   ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 27),
-                child: Text("عدد التسبيحات",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodySmall),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 26.0),
-                child: Center(
-                  child: InkWell(
-                    onTap: () {},
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return Container(
-                          width: constraints.maxWidth * 0.2, // 20% of the screen width
-                          decoration: BoxDecoration(
-                              color: app_colors.brown,
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Center(
-                              child: Text("$Counter",
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Center(
+                        child: InkWell(
+                          onTap: () {
+                            Counter = 0;
+                            setState(() {});
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: app_colors.brown,
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Text("اعاده البدء",
+                                  style:  Theme.of(context).textTheme.bodySmall),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Center(
+                        child: InkWell(
+                          onTap: () {
+                            onTap();
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: app_colors.brown,
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Text("اضغط",
                                   style: Theme.of(context).textTheme.bodySmall),
                             ),
                           ),
-                        );
-                      },
-                    )
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0),
-                    child: Center(
-                      child: InkWell(
-                        onTap: () {
-                          Counter = 0;
-                          setState(() {});
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: app_colors.brown,
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Text("اعاده البدء",
-                                style:  Theme.of(context).textTheme.bodySmall),
-                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0),
-                    child: Center(
-                      child: InkWell(
-                        onTap: () {
-                          onTap();
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: app_colors.brown,
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Text("اضغط",
-                                style: Theme.of(context).textTheme.bodySmall),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
 
-                ],
-              ),
-            ],
-          ),
+                  ],
+                )
+              ],
+            ),
+          ],
         ),
-        Expanded(child: SizedBox())
-      ],
+      ),
     );
   }
 
